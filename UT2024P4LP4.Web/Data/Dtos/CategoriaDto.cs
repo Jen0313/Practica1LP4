5e250454af4adb71aff62387dd4e09f0a5515a55
Jennifer <Jennifer@DESKTOP-H2PSBI3>
2024-11-04 23:07:33 -0400

    Agregar archivos de proyecto.

---
+++ UT2024P4LP4.Web/Data/Dtos/CategoriaDto.cs
@@ -0,0 +1,17 @@
+﻿namespace UT2024P4LP4.Web.Data.Dtos;
+
+public class CategoriaDto
+{
+	public int Id { get; set; }
+	public string Nombre { get; set; } = null!;
+	public CategoriaRequest ToRequest() => new CategoriaRequest() { Nombre = Nombre, Id = Id };
+
+}
+
+public class CategoriaRequest
+{
+	public int Id { get; set; }
+	public string Nombre { get; set; } = null!;
+
+}
+
---
+++ UT2024P4LP4.Web/Services/CategoriaService.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UT2024P4LP4.Web.Data;
+using UT2024P4LP4.Web.Data.Dtos;
+using UT2024P4LP4.Web.Data.Entities;
+
+namespace UT2024P4LP4.Web.Services;
+
+public interface ICategoriaService
+{
+	Task<Result> Create(CategoriaRequest categoria);
+	Task<Result> Delete(int Id);
+	Task<ResultList<CategoriaDto>> GetAll(CancellationToken cancellationToken = default);
+	Task<Result> Update(CategoriaRequest categoria);
+}
+
+public class CategoriaService(IApplicationDbContext context) : ICategoriaService
+{
+	public async Task<ResultList<CategoriaDto>> GetAll(CancellationToken cancellationToken = default)
+	{
+		var categorias = await context.Categorias
+		.Select(x => x.ToDto())
+		.ToListAsync(cancellationToken);
+		return ResultList<CategoriaDto>.Success(categorias);
+	}
+
+	public async Task<Result> Create(CategoriaRequest categoria)
+	{
+		try
+		{
+			var entity = Categoria.Create(categoria.Nombre);
+			context.Categorias.Add(entity);
+			await context.SaveChangesAsync();
+			return Result.Success("✅Categoria Registrada con exito!");
+		}
+		catch (Exception Ex)
+		{
+			return Result.Failure($"☠️ Error: {Ex.Message}");
+		}
+	}
+	public async Task<Result> Update(CategoriaRequest categoria)
+	{
+		try
+		{
+			var entity = context.Categorias.Where(c => c.Id == categoria.Id).FirstOrDefault();
+			if (entity == null)
+				return Result.Failure($"La categoria '{categoria.Id}' no existe!");
+			if (entity.Update(categoria.Nombre))
+			{
+				await context.SaveChangesAsync();
+				return Result.Success("✅Catgoria modificada con exito!");
+			}
+			return Result.Success("🐫 No has realizado ningun cambio!");
+		}
+		catch (Exception Ex)
+		{
+			return Result.Failure($"☠️ Error: {Ex.Message}");
+		}
+	}
+	public async Task<Result> Delete(int Id)
+	{
+		try
+		{
+			var entity = context.Categorias.Where(c => c.Id == Id).FirstOrDefault();
+			if (entity == null)
+				return Result.Failure($"La categoria '{Id}' no existe!");
+			context.Categorias.Remove(entity);
+			await context.SaveChangesAsync();
+			return Result.Success("✅Categoria eliminada con exito!");
+		}
+		catch (Exception Ex)
+		{
+			return Result.Failure($"☠️ Error: {Ex.Message}");
+		}
+	}
+
+}
